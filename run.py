6f1d9e7c825d623e67392686b3952d94db4e643c
Alessio De Palma <alessiodepalma84@gmail.com>
2024-09-07 18:21:58 +0000

    Create simple app to add item to buy that stop when done and return final shopping list

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def get_shopping_list():
+
+    #create a list to keep new items
+    grocery_shopping_list = []
+
+
+    #guide on how to use this simple shopping list app    
+    print("Type what do I need to buy")
+    print("Enter 'DONE' to stop adding items")
+
+    while True:
+            #enter new items
+            new_item = input("> ")
+
+            #list complete and end the app
+            if new_item == 'DONE':
+                break
+            #add items to shopping list
+            grocery_shopping_list.append(new_item)
+
+    #introduce shopping list
+    print("My grocery shopping list:")
+
+    #print out items from final shopping list
+    for item in grocery_shopping_list:
+        print(item)
+
+get_shopping_list()
